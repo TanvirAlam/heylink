339034e38acfabd25e1a38c8a7e08ca329b7cd55
TAlam <tanvir.alam.shawn@gmail.com>
2021-11-27 20:56:26 +0100

    Adding env support

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
+import dotenv from "dotenv";
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import db from './models';
+dotenv.config();
 
 const app = express();
 const corsOptions = {
